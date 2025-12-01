bdc355e214ae8430612b4d2b53cdc3fcd3d4401a
PKMDroid <princekibali222@gmail.com>
2025-12-01 07:10:25 +0300

    updates

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,33 @@
-class Pet:
-    pass
-
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner is self]
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("pet must be a Pet instance")
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
+
+
+class Pet:
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception("invalid pet type")
+
+        self.name = name
+        self.pet_type = pet_type
+
+        if owner is not None and not isinstance(owner, Owner):
+            raise Exception("owner must be an Owner instance")
+
+        self.owner = owner
+
+        Pet.all.append(self)
